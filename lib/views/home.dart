d0b1be1da4ab7448652b9f0e16492d0f4488c8cd
lmquannnn <leminhquan21199@gmail.com>
2020-07-22 11:45:32 +0700

    Add bottom navigation bar

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -10,88 +10,195 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   AnimationController _animationController;
+  int _selectedIndex = 0;
 
   @override
   void initState() {
     super.initState();
     _animationController =
-    new AnimationController(vsync: this, duration: Duration(seconds: 5))
-      ..repeat();
+        new AnimationController(vsync: this, duration: Duration(seconds: 5))
+          ..repeat();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Material(
-        child: Stack(
-          fit: StackFit.passthrough,
-          children: <Widget>[
-            Image.asset('assets/images/background.jpg', fit: BoxFit.cover),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 16, horizontal: 10),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  PointRowTop(context, 0, 'assets/images/one.png'),
-                  PointRowTop(context, 0, 'assets/images/heart.png'),
-                  PointRowTop(context, 0, 'assets/images/note.png'),
-                  PointRowTop(context, 0, 'assets/images/gems.png'),
-                ],
+      child: Scaffold(
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          child: Stack(
+            fit: StackFit.passthrough,
+            children: [
+              Image.asset('assets/images/background.jpg', fit: BoxFit.cover),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    PointRowTop(context, 0, 'assets/images/one.png'),
+                    PointRowTop(context, 0, 'assets/images/heart.png'),
+                    PointRowTop(context, 0, 'assets/images/note.png'),
+                    PointRowTop(context, 0, 'assets/images/gems.png'),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 100),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    AnimatedBuilder(
-                      animation: _animationController,
-                      builder: (_, child) {
-                        return Transform.rotate(
-                          angle: _animationController.value * 1 * 3.14,
-                          child: child,
-                        );
-                      },
-                      child: Image.asset('assets/images/disk.png'),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 10),
-                      child: Text(
-                        '1. BigCity Boi',
-                        style: TextStyle(
-                            fontSize: 30,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Container(
-                      width: 200,
-                      height: 70,
-                      margin: EdgeInsets.only(top: 20),
-                      child: FlatButton(
-                        onPressed: () {
-                          Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => GamePlay()));
-                        },
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(70),
-                            side: BorderSide(color: Colors.white, width: 3)),
-                        child: Text(
-                          'Play',
-                          style: TextStyle(fontSize: 25, color: Colors.white),
+              Container(
+                  margin: EdgeInsets.only(top: 100),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        AnimatedBuilder(
+                          animation: _animationController,
+                          builder: (_, child) {
+                            return Transform.rotate(
+                              angle: _animationController.value * 1 * 3.14,
+                              child: child,
+                            );
+                          },
+                          child: Image.asset('assets/images/disk.png'),
                         ),
-                        color: Colors.white24,
-                      ),
-                    ),
-                  ]),
-            )
+                        Container(
+                          margin: EdgeInsets.only(top: 10),
+                          child: Text(
+                            '1. BigCity Boi',
+                            style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Container(
+                            width: 200,
+                            height: 70,
+                            margin: EdgeInsets.only(top: 20),
+                            child: FlatButton(
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => GamePlay()));
+                              },
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(70),
+                                  side:
+                                      BorderSide(color: Colors.white, width: 3)),
+                              child: Text(
+                                'Play',
+                                style:
+                                    TextStyle(fontSize: 25, color: Colors.white),
+                              ),
+                              color: Colors.white24,
+                            ))
+                      ]))
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          iconSize: 28,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.music_note),
+              title: Text('Songs'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('Profile'),
+            ),
           ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.white,
+          backgroundColor: Colors.lightBlue[900],
+          onTap: _onItemTapped,
         ),
       ),
     );
   }
+//  @override
+//  Widget build(BuildContext context) {
+//    return SafeArea(
+//      child: Material(
+//        child: Stack(
+//          fit: StackFit.passthrough,
+//          children: <Widget>[
+//            Image.asset('assets/images/background.jpg', fit: BoxFit.cover),
+//            Container(
+//              margin: EdgeInsets.symmetric(vertical: 16, horizontal: 10),
+//              child: Row(
+//                crossAxisAlignment: CrossAxisAlignment.start,
+//                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                children: <Widget>[
+//                  PointRowTop(context, 0, 'assets/images/one.png'),
+//                  PointRowTop(context, 0, 'assets/images/heart.png'),
+//                  PointRowTop(context, 0, 'assets/images/note.png'),
+//                  PointRowTop(context, 0, 'assets/images/gems.png'),
+//                ],
+//              ),
+//            ),
+//            Container(
+//              margin: EdgeInsets.only(top: 100),
+//              child: Column(
+//                  mainAxisAlignment: MainAxisAlignment.start,
+//                  crossAxisAlignment: CrossAxisAlignment.center,
+//                  children: [
+//                    AnimatedBuilder(
+//                      animation: _animationController,
+//                      builder: (_, child) {
+//                        return Transform.rotate(
+//                          angle: _animationController.value * 1 * 3.14,
+//                          child: child,
+//                        );
+//                      },
+//                      child: Image.asset('assets/images/disk.png'),
+//                    ),
+//                    Container(
+//                      margin: EdgeInsets.only(top: 10),
+//                      child: Text(
+//                        '1. BigCity Boi',
+//                        style: TextStyle(
+//                            fontSize: 30,
+//                            color: Colors.white,
+//                            fontWeight: FontWeight.bold),
+//                      ),
+//                    ),
+//                    Container(
+//                      width: 200,
+//                      height: 70,
+//                      margin: EdgeInsets.only(top: 20),
+//                      child: FlatButton(
+//                        onPressed: () {
+//                          Navigator.pushReplacement(
+//                              context,
+//                              MaterialPageRoute(
+//                                  builder: (context) => GamePlay()));
+//                        },
+//                        shape: RoundedRectangleBorder(
+//                            borderRadius: BorderRadius.circular(70),
+//                            side: BorderSide(color: Colors.white, width: 3)),
+//                        child: Text(
+//                          'Play',
+//                          style: TextStyle(fontSize: 25, color: Colors.white),
+//                        ),
+//                        color: Colors.white24,
+//                      ),
+//                    ),
+//                  ]),
+//            ),
+//          ],
+//        ),
+//      ),
+//    );
+//  }
 }
