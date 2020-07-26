1a4207d6068461f822c88a33631aa29824117711
ttnam99 <51222429+ttnam99@users.noreply.github.com>
2020-07-26 14:04:47 +0700

    Commit #02 - Changes to menus

--- lib/views/music_list.dart
+++ lib/views/music_list.dart
@@ -1,5 +1,6 @@
+import 'dart:core';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:piano_tile/views/home.dart';
 
@@ -15,62 +16,76 @@ class MusicList extends StatelessWidget {
           primaryColor: Colors.blue
       ),
       home: NotificationListener<OverscrollIndicatorNotification>(// để che hiệu ứng glow khi cuộn ở 2 đầu
-      onNotification: (OverscrollIndicatorNotification overscroll) {
-      overscroll.disallowGlow(); return true;
-      },child:DefaultTabController(
+        onNotification: (OverscrollIndicatorNotification overscroll) {
+          overscroll.disallowGlow(); return true;
+        },child:DefaultTabController(
         length: 3,
         child:Scaffold(
             resizeToAvoidBottomInset: false,
             backgroundColor:  Color(0xFF373737),
-          appBar: TabBar(
-            labelStyle: TextStyle(
-              fontWeight: FontWeight.w900,
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(60.0), // here the desired height
+              child:AppBar(// bỏ comment để hiện thanh tài nguyên trên cùng màn hình
+                /*title:Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  PointRowTop(context, 0, 'assets/images/one.png'),
+                  PointRowTop(context, 0, 'assets/images/heart.png'),
+                  PointRowTop(context, 0, 'assets/images/note.png'),
+                  PointRowTop(context, 0, 'assets/images/gems.png'),
+                ],
+              ),*/
+                bottom: TabBar(
+                  labelStyle: TextStyle(
+                    fontWeight: FontWeight.w900,
+                  ),
+                  indicatorColor: Colors.white12,
+                  indicatorWeight: 10.0,
+                  labelColor: Colors.white,
+                  tabs: [
+                    Tab(child: Align(
+                      alignment: Alignment.center,
+                      child: Text("Nhạc Việt"),
+                    ),),
+                    Tab(child: Align(
+                      alignment: Alignment.center,
+                      child: Text("Nhạc\nNước Ngoài",textAlign: TextAlign.center,),
+                    ),
+                    ),
+                    Tab(child: Align(
+                      alignment: Alignment.center,
+                      child: Text("Yêu thích",textAlign: TextAlign.center,),
+                    ),),
+                  ],
+                ),
+              ),),
+            body: TabBarView(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/background.jpg'), fit: BoxFit.cover)
+                    ),
+                    child:BodyLayout(0)),
+                Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/background.jpg'), fit: BoxFit.cover)
+                    ),
+                    child:BodyLayout(0)),
+                Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/background.jpg'), fit: BoxFit.cover)
+                    ),
+                    child:BodyLayout(0)),
+              ],
             ),
-            indicatorColor: Colors.white12,
-            indicatorWeight: 10.0,
-            labelColor: Colors.white,
-            tabs: [
-             Tab(child: Align(
-               alignment: Alignment.center,
-               child: Text("Nhạc Việt"),
-             ),),
-             Tab(child: Align(
-               alignment: Alignment.center,
-               child: Text("Nhạc\nNước Ngoài",textAlign: TextAlign.center,),
-             ),
-             ),
-             Tab(child: Align(
-               alignment: Alignment.center,
-               child: Text("Yêu thích",textAlign: TextAlign.center,),
-             ),),
-            ],
-          ),
-          body: TabBarView(
-            children: [
-              Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/background.jpg'), fit: BoxFit.cover)
-                  ),
-                  child:BodyLayout(0)),
-              Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/background.jpg'), fit: BoxFit.cover)
-                  ),
-                  child:BodyLayout(0)),
-              Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/background.jpg'), fit: BoxFit.cover)
-                  ),
-                  child:BodyLayout(0)),
-            ],
-          ),
-          bottomNavigationBar: BottomNavLayout()
+            bottomNavigationBar: BottomNavLayout()
         ),
       ),
-    ),);
+      ),);
   }
 }
 
@@ -101,42 +116,42 @@ Widget songsListView(BuildContext context,tabIndex) {
   //final difficulty=data[4];
   return NotificationListener<OverscrollIndicatorNotification>(// tắt hiệu ứng glow khi cuộn tới cuối list/ đầu list
     onNotification: (OverscrollIndicatorNotification overscroll) {
-    overscroll.disallowGlow(); return true;
+      overscroll.disallowGlow(); return true;
     },
     child: ListView.builder(
       itemCount: titles.length,
       itemBuilder: (context, index) {
-      return GestureDetector(
-        onTap: () {
-          final snackBar = SnackBar(content: Text(titles[index]));
-          Scaffold.of(context).showSnackBar(snackBar);
-        },
-        child:Card(
-          child: ListTile(
-            leading:Container(
-              height: double.infinity,
-              child:Icon(icons[index], size:40)),
-            title: Text(titles[index]),
-            subtitle: Text(artists[index]+"\n"+'BPM:'),
-            isThreeLine: true,
-            trailing:  RaisedButton(
-              color: Colors.amber,
-              child: Text("Chơi"),
-              shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-              onPressed: () => {
-                Navigator.push(context,MaterialPageRoute(builder: (context) => Home())
-              )},
-            ),
-          ),
-        )
-      );
+        return GestureDetector(
+            onTap: () {
+              final snackBar = SnackBar(content: Text(titles[index]));
+              Scaffold.of(context).showSnackBar(snackBar);
+            },
+            child:Card(
+              child: ListTile(
+                leading:Container(
+                    height: double.infinity,
+                    child:Icon(icons[index], size:40)),
+                title: Text(titles[index]),
+                subtitle: Text(artists[index]+"\n"+'BPM:'),
+                isThreeLine: true,
+                trailing:  RaisedButton(
+                  color: Colors.amber,
+                  child: Text("Chơi"),
+                  shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                  onPressed: () => {
+                    Navigator.push(context,MaterialPageRoute(builder: (context) => Home())
+                    )},
+                ),
+              ),
+            )
+        );
       },
-  ),);
-
+    ),);
 }
 Widget bottomNav(BuildContext context) {
   return BottomNavigationBar(
-    backgroundColor: Colors.white12,
+    selectedItemColor: Colors.white,
+    backgroundColor: Colors.lightBlue[900],
     currentIndex: 1, // this will be set when a new tab is tapped
     items: [
       BottomNavigationBarItem(
@@ -169,4 +184,12 @@ List getSongs(tabIndex){
   final icons = [Icons.music_note, Icons.music_note,Icons.music_note,Icons.music_note,
     Icons.music_note,Icons.music_note,Icons.music_note,Icons.music_note,Icons.music_note];
   return [titles,artists,icons];
+}
+class Song{
+  String id;
+  String name;
+  String bpm;
+  List<String> artists;
+  double difficulty;
+
 }
