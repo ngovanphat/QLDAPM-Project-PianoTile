79b5318249724529b24b00c3fa4ec491afdae756
ttnam99 <51222429+ttnam99@users.noreply.github.com>
2020-08-04 10:57:39 +0700

    Commit #06 - Change to ExpandablePanel, Change Text to English

--- lib/views/music_list.dart
+++ lib/views/music_list.dart
@@ -1,4 +1,5 @@
 import 'dart:core';
+import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +9,7 @@ import 'package:piano_tile/views/home.dart';
 import 'package:smooth_star_rating/smooth_star_rating.dart';
 import 'package:marquee_flutter/marquee_flutter.dart';
 import 'package:piano_tile/model/widget.dart';
+import 'package:like_button/like_button.dart';
 
 import 'game_play.dart';
 
@@ -47,15 +49,21 @@ class _MusicListState extends State<MusicList> {
                     Tab(
                       child: Align(
                         alignment: Alignment.center,
-                        child: Text("Nhạc Việt"),
+                        child: Text("Vietnamese",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),),
                       ),
                     ),
                     Tab(
                       child: Align(
                         alignment: Alignment.center,
                         child: Text(
-                          "Nhạc\nNước Ngoài",
+                          "Foreign",
                           textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
                         ),
                       ),
                     ),
@@ -63,8 +71,11 @@ class _MusicListState extends State<MusicList> {
                       child: Align(
                         alignment: Alignment.center,
                         child: Text(
-                          "Yêu thích",
+                          "Favorited",
                           textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
                         ),
                       ),
                     ),
@@ -83,15 +94,15 @@ class _MusicListState extends State<MusicList> {
                 Container(
                     decoration: BoxDecoration(
                         image: DecorationImage(
-                            image: AssetImage('assets/images/background.jpg'),
-                            fit: BoxFit.cover)),
-                    child: BodyLayout(0)),
+                            image: AssetImage('assets/images/background.jpg'), fit: BoxFit.cover)
+                    ),
+                    child:BodyLayout(1)),
                 Container(
                     decoration: BoxDecoration(
                         image: DecorationImage(
-                            image: AssetImage('assets/images/background.jpg'),
-                            fit: BoxFit.cover)),
-                    child: BodyLayout(0)),
+                            image: AssetImage('assets/images/background.jpg'), fit: BoxFit.cover)
+                    ),
+                    child:BodyLayout(2)),
               ],
             ),
           ),
@@ -100,18 +111,274 @@ class _MusicListState extends State<MusicList> {
   }
 }
 
-class BodyLayout extends StatelessWidget {
+class BodyLayout extends StatefulWidget {
   int tabIndex;
-  BodyLayout(int tabIndex) {
-    this.tabIndex = tabIndex;
+  //BodyLayout({Key key, this.tabIndex}) : super(key: key);
+  BodyLayout(int tabIndex){
+    this.tabIndex=tabIndex;
   }
+
+  @override
+  _BodyLayoutState createState() => _BodyLayoutState(tabIndex);
+}
+
+class _BodyLayoutState extends State<BodyLayout> {
+  int tabIndex;
+  //danh sách bài hát
+  List<Song> songs=[];
+  int selected = -1; //attention
+  int visileItems=8;
+  final scrollController = ScrollController();
+  double cardHeight=70;
+  final GlobalKey datakey=GlobalKey();
+  _BodyLayoutState(tabIndex){
+    this.tabIndex=tabIndex;
+
+  }
+
+  @override
+  void initState() {
+    songs= getSongs(tabIndex);
+  }
+
+ /* @override
+  Widget build(BuildContext context) {
+    return NotificationListener<OverscrollIndicatorNotification>(// tắt hiệu ứng glow khi cuộn tới cuối list/ đầu list
+      onNotification: (OverscrollIndicatorNotification overscroll) {
+        overscroll.disallowGlow(); return true;
+      },
+      child: ListView.builder(
+        *//*key: Key('builder ${selected.toString()}'),*//*
+        key:datakey,
+        controller: scrollController,
+        itemCount: songs.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+              onTap: () {
+                final snackBar = SnackBar(content: Text(songs[index].getName()));//phát bài nhạc để nghe thử khi nhấn vào/show thành tích
+                Scaffold.of(context).showSnackBar(snackBar);
+              },
+              child:Card(
+                child: ExpansionTile(
+                  //isThreeLine: true,
+                  key: Key(index.toString()), //attention
+                  initiallyExpanded : index==selected, //attention
+                  onExpansionChanged: ((newState){
+                    if(newState)
+                      setState(() {
+                        Duration(seconds:  20000);
+                        selected = index;
+                      });
+                    else setState(() {
+                      selected = -1;
+                    });
+                  }),
+                  leading:Container(
+                    height: double.infinity,
+                    child:ImageIcon(
+                      AssetImage('assets/images/music-note.png'),
+                      size: 50,
+                      color: Color(0xFF3A5A98),
+                    ),//replaced by image if available
+                  ),
+                  title: Text(songs[index].getName()),
+                  subtitle:  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Flexible(
+                          flex:3,
+                          child:Container(
+                            height: 30,
+                            child: MarqueeWidget(
+                              text:songs[index].getArtists().join('-'),
+                              textStyle: TextStyle(fontSize: 16.0),
+                              scrollAxis: Axis.horizontal,),
+                            //Text(,overflow: TextOverflow.ellipsis,),
+                          ),),
+                        Flexible(
+                          flex: 3,
+                          child:SmoothStarRating(
+                            rating: songs[index].getDifficulty().toDouble(),
+                            size: 18,
+                            filledIconData: Icons.music_note,
+                            defaultIconData: null,
+                            starCount: 5,
+                            isReadOnly: true,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Flexible(
+                          flex:5,
+                          child:Container(
+                            height: 30,
+                            padding: EdgeInsets.symmetric(horizontal:15.0),
+                            child: Text('Thành tích : '+songs[index].getHighscore().toString(),
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,fontSize: 17,color: Colors.lightBlue[900].withOpacity(0.8)
+                                ))
+                            //Text(,overflow: TextOverflow.ellipsis,),
+                          ),),
+                        Flexible(
+                          flex: 2,
+                          child:LikeButton(
+                            size:50,
+                            bubblesSize: 50,
+                            circleSize: 30,
+                            circleColor: CircleColor(start: Colors.amber, end: Colors.redAccent),
+                            bubblesColor: BubblesColor(
+                              dotPrimaryColor: Colors.redAccent,
+                              dotSecondaryColor: Colors.amber,
+                            ),
+                            likeBuilder: (bool isLiked) {
+                              return Icon(
+                                Icons.favorite,
+                                color: isLiked ? Colors.redAccent : Colors.grey,
+                                size: 30,
+                              );
+                            },
+                          )
+                        )
+                      ],
+                    ),
+                  ],
+                  trailing:  RaisedButton(
+                    color: Colors.amber,
+                    child: Text("Chơi"),
+                    shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                    onPressed: () => {
+                      Navigator.push(context,MaterialPageRoute(builder: (context) => Home())
+                      )},
+                  ),
+                ),
+              )
+          );
+        },
+      ),);
+  }*/
   @override
   Widget build(BuildContext context) {
-    return songsListView(context, tabIndex);
+    return NotificationListener<OverscrollIndicatorNotification>(// tắt hiệu ứng glow khi cuộn tới cuối list/ đầu list
+        onNotification: (OverscrollIndicatorNotification overscroll) {
+          overscroll.disallowGlow(); return true;
+        },
+        child: ListView(
+          children: <Widget>[
+            Container(
+              child: _buildPanel(),
+            ),
+          ],
+        ),
+    );
+  }
+  Widget _buildPanel() {
+    return ExpansionPanelList.radio(
+      initialOpenPanelValue: 2,
+      children: songs.map<ExpansionPanelRadio>((Song song) {
+        return ExpansionPanelRadio(
+            value: song.id,
+            canTapOnHeader: true,
+            headerBuilder: (BuildContext context, bool isExpanded) {//Header
+              return ListTile(
+                leading:Container(
+                  height: double.infinity,
+                  child:ImageIcon(
+                    AssetImage('assets/images/music-note.png'),
+                    size: 50,
+                    color: Color(0xFF3A5A98),
+                  ),//replaced by image if available
+                ),
+                title: Text(song.getName()),
+                subtitle:  Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Flexible(
+                        flex:3,
+                        child:Container(
+                          height: 30,
+                          child: MarqueeWidget(
+                            text:song.getArtists().join('-'),
+                            textStyle: TextStyle(fontSize: 16.0),
+                            scrollAxis: Axis.horizontal,),
+                          //Text(,overflow: TextOverflow.ellipsis,),
+                        ),),
+                      Flexible(
+                        flex: 3,
+                        child:SmoothStarRating(
+                          rating: song.getDifficulty().toDouble(),
+                          size: 18,
+                          filledIconData: Icons.music_note,
+                          defaultIconData: null,
+                          starCount: 5,
+                          isReadOnly: true,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+            body: ListTile(
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Flexible(
+                      flex:5,
+                      child:Container(
+                          height: 30,
+                          padding: EdgeInsets.symmetric(horizontal:15.0),
+                          child: Text('Highscore : '+song.getHighscore().toString(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,fontSize: 17,color: Colors.lightBlue[900].withOpacity(0.8)
+                              ))
+                        //Text(,overflow: TextOverflow.ellipsis,),
+                      ),),
+                    Flexible(
+                        flex: 2,
+                        child:LikeButton(
+                          size:50,
+                          bubblesSize: 50,
+                          circleSize: 30,
+                          circleColor: CircleColor(start: Colors.amber, end: Colors.redAccent),
+                          bubblesColor: BubblesColor(
+                            dotPrimaryColor: Colors.redAccent,
+                            dotSecondaryColor: Colors.amber,
+                          ),
+                          likeBuilder: (bool isLiked) {
+                            return Icon(
+                              Icons.favorite,
+                              color: isLiked ? Colors.redAccent : Colors.grey,
+                              size: 30,
+                            );
+                          },
+                        )
+                    )
+                  ],
+                ),
+            trailing: RaisedButton(
+              color: Colors.amber,
+              child: Text("Play"),
+              shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+              onPressed: () => {
+                Navigator.push(context,MaterialPageRoute(builder: (context) => GamePlay())
+                )},
+            ),
+            ),);
+      }).toList(),
+    );
   }
 }
 
-class BottomNavLayout extends StatelessWidget {
+class BottomNavLayout extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return bottomNav(context);
@@ -122,10 +389,7 @@ Widget songsListView(BuildContext context, tabIndex) {
   //tên bài hát
   List<Song> songs = getSongs(tabIndex);
 
-  //final beatRate=data[3];
-  //final difficulty=data[4];
-  return NotificationListener<OverscrollIndicatorNotification>(
-    // tắt hiệu ứng glow khi cuộn tới cuối list/ đầu list
+  return NotificationListener<OverscrollIndicatorNotification>(// tắt hiệu ứng glow khi cuộn tới cuối list/ đầu list
     onNotification: (OverscrollIndicatorNotification overscroll) {
       overscroll.disallowGlow();
       return true;
@@ -268,5 +532,11 @@ List getSongs(tabIndex) {
     musicList.add(new Song(
         i.toString(), titles[i], [artists[i]], difficulties[i], images[i]));
   }
-  return musicList;
+  if(tabIndex==1) {
+    return musicList.reversed.toList();
+  }else if(tabIndex==2){
+    musicList.removeRange(4, 8);
+    return musicList;
+  }
+  else return musicList;
 }
