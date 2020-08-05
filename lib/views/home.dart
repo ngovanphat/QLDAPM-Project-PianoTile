530d90b6e537a19ffcd6903aaa1171ad42aea83a
lmquannnn <leminhquan21199@gmail.com>
2020-08-05 11:47:39 +0700

    Deploy on iOS simulator and set up links to Firebase

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -25,6 +25,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xff004466),
       body: SafeArea(
         child: IndexedStack(index: _currentIndex, children: [
           Container(
