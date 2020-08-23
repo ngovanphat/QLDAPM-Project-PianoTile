08dc0bbbaaa5b96740be2757692f9f1277aa3dbb
lmquannnn <leminhquan21199@gmail.com>
2020-08-23 23:41:18 +0700

    Add friends

--- lib/views/friends_list.dart
+++ lib/views/friends_list.dart
@@ -8,7 +8,17 @@ class FriendsList extends StatefulWidget {
 }
 
 class _FriendsListState extends State<FriendsList> {
+  TextEditingController nameController = TextEditingController();
   List<Friend> _friends = loadFriend();
+
+  void addItemToList() {
+    setState(() {
+      _friends.insert(
+        0,
+        new Friend(nameController.text, '1', 'assets/images/female.png'),
+      );
+    });
+  }
 
   Widget _buildFriendListTile(BuildContext context, int index) {
     var friend = _friends[index];
@@ -130,7 +140,70 @@ class _FriendsListState extends State<FriendsList> {
           onPressed: () => Navigator.of(context).pop(),
         ),
         backgroundColor: const Color(0xff004466),
-        title: new Text('FRIENDS'),
+        title: Center(
+          child: new Text('FRIENDS'),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add, color: Colors.white, size: 30),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: BorderSide(color: Colors.black, width: 2),
+                    ),
+                    backgroundColor: Colors.white,
+                    title: Center(
+                      child: Text(
+                        "Add Friend?",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    content: TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'User Name',
+                      ),
+                    ),
+                    actions: [
+                      FlatButton(
+                        child: Text("Send Request"),
+                        onPressed: () {
+                          addItemToList();
+                          nameController.clear();
+                          Navigator.of(context).pop();
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                Future.delayed(Duration(seconds: 1), () {
+                                  Navigator.of(context).pop(true);
+                                });
+                                return AlertDialog(
+                                    shape: CircleBorder(
+                                      side: BorderSide(
+                                          color: Colors.white, width: 2),
+                                    ),
+                                    backgroundColor: const Color(0xff00ff00),
+                                    content: Container(
+                                        child: Icon(
+                                      Icons.done,
+                                      color: Colors.white,
+                                      size: 50,
+                                    )));
+                              });
+                        },
+                      )
+                    ],
+                  );
+                },
+              );
+            },
+          )
+        ],
       ),
       body: SafeArea(
         child: Container(
