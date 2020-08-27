4c89e12ab8aad0cc13aacee0a0605d2ebf881576
qttq23 <1753102@student.hcmus.edu.vn>
2020-08-27 09:06:34 +0700

    update app init: get exp, gem then save to local file, test ok with guest

---
+++ lib/helper/sharedPreferencesDefinition.dart
@@ -0,0 +1,44 @@
+
+
+
+class sharedPrefKeys {
+
+  static String userType = "userType";
+
+  // guest
+  static String guestId = "guestId";
+
+  static String guestName = "guestName";
+
+  static String guestExp = "guestExp";
+
+  static String guestGem = "guestGem";
+
+  static String guestLevel = "guestLevel";
+
+  // room
+  static String roomId = "roomId";
+
+  static String isRoomHost = "isRoomHost";
+
+  // logged-in user
+  static String userId = "userId";
+
+  static String userName = "userName";
+
+  static String userExp = "userExp";
+
+  static String userGem = "userGem";
+
+  static String userLevel = "userLevel";
+
+
+}
+
+class sharedPrefValues {
+
+  // user type
+  static int GUEST = 0;
+  static int USER = 1;
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,61 @@ import 'package:piano_tile/views/home.dart';
 import 'package:piano_tile/views/game_play.dart';
 import 'package:piano_tile/views/game_play_online.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:piano_tile/helper/sharedPreferencesDefinition.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+void main() async {
+
+  // if change main to async
+  // app needs this line to run properly
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // check if guest or logged-in user
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  int type = prefs.getInt(sharedPrefKeys.userType) ?? sharedPrefValues.GUEST;
+  int exp = null;
+  int gem = null;
+
+  if(type == sharedPrefValues.USER){
+
+    // get info from firebase database
+    String userId = prefs.getString(sharedPrefKeys.userId);
+    DataSnapshot user = await FirebaseDatabase.instance.reference()
+        .child('account/$userId')
+        .once();
+
+    if(user == null) {
+      type = sharedPrefValues.GUEST;
+      print('[main] get_user_from_firebase: failed, changed type to guest');
+    }
+    else {
+      print('[main] user: $user');
+      Map<dynamic, dynamic> rows = user.value;
+      // get exp, gems
+      exp = rows['exp'];
+      gem = rows['gem'];
+
+      // save exp, gem
+      prefs.setInt(sharedPrefKeys.userExp, exp);
+      prefs.setInt(sharedPrefKeys.userGem, gem);
+    }
+
+  }
+
+  if(type == sharedPrefValues.GUEST){
+    // type == GUEST
+
+    // get info from local preferences
+    int exp = prefs.getInt(sharedPrefKeys.guestExp) ?? 0;
+    int gem = prefs.getInt(sharedPrefKeys.guestGem) ?? 0;
+    print('[main] local exp: $exp, gem: $gem');
+
+    // save exp, gem
+    prefs.setInt(sharedPrefKeys.guestExp, exp);
+    prefs.setInt(sharedPrefKeys.guestGem, gem);
+  }
 
 
-void main() {
   runApp(MyApp());
 }
 
